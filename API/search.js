7134b389b050b85dce676055c171c8654c96999c
AlexMarvelo <mironenkoalexey@gmail.com>
2016-10-16 18:38:52 +0300

    enable rerequeting to OMDb

--- API/search.js
+++ API/search.js
@@ -1,9 +1,19 @@
 var fetch = require('node-fetch');
+var OMDbURL = 'http://www.omdbapi.com';
 
-function getSearchRequestData() {
-  return fetch('http://www.omdbapi.com/?s=iron+man')
+function getSearchRequestData(jObj) {
+  return fetch(`${OMDbURL}/${getQString(jObj)}`)
     .then(res => res.text())
     .then(JSON.parse);
+}
+
+function getQString(jObj) {
+  let qString = '?';
+  for (let prop in jObj) {
+    qString += `${prop}=${jObj[prop]}&`;
+  }
+  qString = qString.slice(0, -1).replace(' ', '+');
+  return qString;
 }
 
 module.exports = getSearchRequestData;
