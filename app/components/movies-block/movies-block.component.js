9f43635b0fb64d6c16ba37d7824cf815b5a91392
AlexMarvelo <mironenkoalexey@gmail.com>
2016-10-20 18:06:01 +0300

    fix favourites toggling

--- app/components/movies-block/movies-block.component.js
+++ app/components/movies-block/movies-block.component.js
@@ -43,33 +43,16 @@ angular.
           }
         };
 
-        this.onPaginationClick = (event) => {
+        this.onPaginationClick = (event, page) => {
           event.preventDefault();
-          let targetPage = parseInt(event.target.dataset.id);
-          if (!targetPage) return;
-          Movies.loadMovies(null, targetPage);
+          if (!page) return;
+          Movies.loadMovies(null, page.number);
         };
 
-        this.onFavouritesAddClick = (event) => {
-          if (!event.target.classList.contains('btn-favourite') &&
-              !event.target.classList.contains('btn-favourite-text') &&
-              !event.target.classList.contains('glyphicon-star')) return;
+        this.onFavouritesAddClick = (event, movie) => {
           event.preventDefault();
-          let card = window.findAncestor(event.target, 'thumbnail');
-          if (!card || !card.dataset.id) return;
-          if (!card.classList.contains('thumbnail-favourite')) {
-            card.classList.add('thumbnail-favourite');
-            let movieIndex = this.currentView.Search.indexOfByProp({imdbID: card.dataset.id}, 'imdbID');
-            let selectedMovie = this.currentView.Search[movieIndex];
-            selectedMovie.isFavourite = true;
-            this.favourites.push(selectedMovie);
-          } else {
-            card.classList.remove('thumbnail-favourite');
-            let movieIndex = this.favourites.indexOfByProp({imdbID: card.dataset.id}, 'imdbID');
-            if (card.dataset.id && movieIndex !== -1) {
-              this.favourites.splice(movieIndex, 1);
-            }
-          }
+          if (!movie) return;
+          movie.isFavourite = !movie.isFavourite;
           localStorageService.set('favourites', this.favourites);
         };
       }
@@ -80,40 +63,40 @@ angular.
     },
 
     template: `
-    <search-block></search-block>
+      <search-block></search-block>
 
-    <div class="container" ng-click="$ctrl.onFavouritesAddClick($event)">
+      <div class="container">
 
-      <div class="row text-center">
-        <div ng-repeat="movie in $ctrl.currentView.Search" class="movie-card col-sm-6 col-md-4 col-lg-3">
-          <div class="thumbnail {{movie.isFavourite ? 'thumbnail-favourite' : ''}}" data-id="{{movie.imdbID}}">
-            <a ui-sref="movieDetails({movieID: movie.imdbID})">
-              <img ng-src="{{movie.Poster !== 'N/A' ? movie.Poster : 'http://placehold.it/280x390'}}" alt="{{movie.Title}}">
-            </a>
-            <div class="caption">
+        <div class="row text-center">
+          <div ng-repeat="movie in $ctrl.currentView.Search" class="movie-card col-sm-6 col-md-4 col-lg-3">
+            <div class="thumbnail {{movie.isFavourite ? 'thumbnail-favourite' : ''}}" data-id="{{movie.imdbID}}">
               <a ui-sref="movieDetails({movieID: movie.imdbID})">
-                <h3>{{movie.Title}}</h3>
+                <img ng-src="{{movie.Poster !== 'N/A' ? movie.Poster : 'http://placehold.it/280x390'}}" alt="{{movie.Title}}">
               </a>
-              <span class="badge">{{movie.Year}}</span>
-              <span class="badge">{{movie.Type}}</span>
-              <button class="btn btn-default btn-favourite" type="button">
-                <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
-                <span class="btn-favourite-text btn-favourite-text-add">Add to favourites</span>
-                <span class="btn-favourite-text btn-favourite-text-remove">Remove from favourites</span>
-              </button>
+              <div class="caption">
+                <a ui-sref="movieDetails({movieID: movie.imdbID})">
+                  <h3>{{movie.Title}}</h3>
+                </a>
+                <span class="badge">{{movie.Year}}</span>
+                <span class="badge">{{movie.Type}}</span>
+                <button class="btn btn-default btn-favourite" type="button" ng-click="$ctrl.onFavouritesAddClick($event, movie)">
+                  <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+                  <span class="btn-favourite-text btn-favourite-text-add">Add to favourites</span>
+                  <span class="btn-favourite-text btn-favourite-text-remove">Remove from favourites</span>
+                </button>
+              </div>
             </div>
           </div>
         </div>
+
+        <nav class="text-center" aria-label="Page navigation">
+          <ul class="pagination" ng-click="$ctrl.onPaginationClick($event)">
+            <li ng-repeat="page in $ctrl.pagination" class="{{page.active ? 'active' : ''}}">
+              <a href="#" ng-click="$ctrl.onPaginationClick($event, page)">{{page.name}}</a>
+            </li>
+          </ul>
+        </nav>
+
       </div>
-
-      <nav class="text-center" aria-label="Page navigation">
-        <ul class="pagination" ng-click="$ctrl.onPaginationClick($event)">
-          <li ng-repeat="page in $ctrl.pagination" class="{{page.active ? 'active' : ''}}">
-            <a href="{{page.link}}" data-id="{{page.number}}">{{page.name}}</a>
-          </li>
-        </ul>
-      </nav>
-
-    </div>
     `,
   });
